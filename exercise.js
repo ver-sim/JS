525b12a4b63052f231a9093dfc81f294cfa4e21f
ver-sim <verdianasimonetti@gmail.com>
2023-10-22 15:06:45 +0200

    es arrow ft

---
+++ exercise.js
@@ -0,0 +1,32 @@
+// const sum = function (a, b) {
+//   return a + b;
+// }
+
+// const subtract = function (a, b) {
+//   return a - b;
+// }
+
+// const multiply = function (a, b) {
+//   return a * b;
+// }
+
+// const divide = function (a, b) {
+//   return a / b;
+// }
+
+// const log = function (value) {
+//   console.log(value);
+// }
+
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const log = (value) => console.log(value);
+
+let i = sum(2, 4);
+let j = sum(5, 2);
+let y = multiply(i, j);
+let x = subtract(y, 2);
+let k = divide(x, 5);
+log(k);
