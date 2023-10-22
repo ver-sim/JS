d8b57e969dc7ba585685be2b0e718fc1efb4eb61
ver-sim <verdianasimonetti@gmail.com>
2023-10-22 14:18:39 +0200

    es name-age

---
+++ exercise.js
@@ -0,0 +1,26 @@
+function nicknameMap(persons) {
+  let nickname = [];
+  for(let i = 0; i < persons.length; i++) {
+    let newPer = persons[i].name + "-" + persons[i].age;
+    nickname.push(newPer);
+  }
+  return nickname;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
+
