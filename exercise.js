d6d9240ff4cc790e98cf582ead3f9f6188749ebe
ver-sim <verdianasimonetti@gmail.com>
2023-10-30 09:48:30 +0100

    es corretto

--- exercise.js
+++ exercise.js
@@ -1,7 +1,5 @@
 const order = {};
 
-// if (order && order.customer && order.customer.address && !order.customer.address.city) {
-//   console.log('City is required');
-// }
-
-let check = order.customer.address?.city;
+if (!order?.customer?.address?.city) {
+  console.log('City is required');
+}
