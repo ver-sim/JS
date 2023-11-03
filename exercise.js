5f7cd67dc2ae4980b2ec3717529bc8bb310005be
ver-sim <verdianasimonetti@gmail.com>
2023-11-03 12:30:10 +0100

    es semplificare codice

---
+++ exercise.js
@@ -0,0 +1,18 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+const personInfo = {...person};
+delete personInfo.id;
+
+
+console.log(id, personInfo); 
