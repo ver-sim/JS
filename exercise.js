e0c28efb31c7ba6bf0ff53cb168613b8bfe150b2
ver-sim <verdianasimonetti@gmail.com>
2023-11-05 22:53:59 +0100

    es fromJsono

---
+++ exercise.js
@@ -0,0 +1,27 @@
+class Person {
+  static json
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+  static fromJson(json){
+   let obj = JSON.parse(json);
+   return new Person(obj.id, obj.firstName, obj.lastName, obj.age)
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+
+const developer = Person.fromJson(json);
+console.log(developer);
+
+
+
+
